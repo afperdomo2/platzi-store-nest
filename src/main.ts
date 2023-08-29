9b481c27661bf62441a18a3d67eaa3897011ecf9
afperdomo2 <alvarado0925@gmail.com>
2023-08-29 17:38:21 -0500

    Ajusta ValidationPipe para que elimine o emita error al recibir propiedades que no estén validadas con decoradores

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,24 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /**
+       * Si se establece en verdadero, el validador eliminará del objeto validado,
+       * cualquier propiedad que no tenga ningún decorador.
+       *
+       * Consejo: si ningún otro decorador es adecuado para tu propiedad, utiliza este:
+       * @Allow — decorator
+       */
+      whitelist: true,
+
+      /**
+       * Si se establece en verdadero, en lugar de eliminar las propiedades que no
+       * están en la lista blanca, el validador lanzará un error.
+       */
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3000);
 }
