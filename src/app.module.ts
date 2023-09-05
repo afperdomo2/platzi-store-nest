80d926b22704a33165bb977fc6ca89097d19cd7a
alvarado0925 <alvarado0925@gmail.com>
2023-09-05 00:35:06 -0500

    Implementando inyecciones asíncronas con useFactory y HttpService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsGroupModule } from './modules/products/products.module';
@@ -8,13 +10,22 @@ const API_KEY = '123456789';
 const API_KEY_PROD = 'ABC123$1';
 
 @Module({
-  imports: [ProductsGroupModule, UsersGroupModule],
+  imports: [HttpModule, ProductsGroupModule, UsersGroupModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const request = http.get('https://jsonplaceholder.typicode.com/todos');
+        const tasks = await lastValueFrom(request);
+        return tasks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
