6509e81ab5d928717b094600e9fe125e6c7b9331
alvarado0925 <alvarado0925@gmail.com>
2023-09-10 14:48:02 -0500

    Instala e implementa node-postgres en el proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,29 @@ import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config/dist';
 import { lastValueFrom } from 'rxjs';
+import { Client } from 'pg';
 
 import { enviroments } from './enviroments';
 import configuration from './config/configuration';
 import envValidations from './config/envValidations';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsGroupModule } from './modules/products/products.module';
 import { UsersGroupModule } from './modules/users/users.module';
 import { DatabaseModule } from './database/database.module';
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'felipe',
+  password: '123456',
+  database: 'platzi_store_nestjs',
+});
+client.connect();
+client.query('SELECT * FROM tasks', (err, res) => {
+  console.error('error:', err);
+  console.info('success:', res.rows);
+});
 
 @Module({
   imports: [
