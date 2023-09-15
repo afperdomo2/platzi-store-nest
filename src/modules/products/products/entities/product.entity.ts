313d864a2b817e346769ba7b37558d2da0f4752a
alvarado0925 <alvarado0925@gmail.com>
2023-09-14 23:23:32 -0500

    Convierte Product Entity en una entidad de TypeORM

--- src/modules/products/products/entities/product.entity.ts
+++ src/modules/products/products/entities/product.entity.ts
@@ -1,8 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
 export class Product {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column('varchar', { length: 255 })
   name: string;
-  description?: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('numeric')
   price: number;
-  stock: number = 0;
-  image?: string;
+
+  @Column('number', { default: 0 })
+  stock: number;
+
+  @Column('varchar')
+  image: string;
 }
