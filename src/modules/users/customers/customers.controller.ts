4a4a109baf3c42c0e5cb7cdcb0547dcffee94a4e
alvarado0925 <alvarado0925@gmail.com>
2023-12-05 23:57:14 -0500

    Agrega documentación y los DTOs al los endpoints de Customers

--- src/modules/users/customers/customers.controller.ts
+++ src/modules/users/customers/customers.controller.ts
@@ -10,7 +10,12 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
@@ -21,22 +26,28 @@ export class CustomersController {
   constructor(private readonly customersService: CustomersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new customer' })
+  @ApiCreatedResponse({ description: 'Customer created successfully' })
   create(@Body() createCustomerDto: CreateCustomerDto) {
     return this.customersService.create(createCustomerDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'List all customers' })
   findAll() {
     return this.customersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find a customer' })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.customersService.findOne(id);
   }
 
   @Patch(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: 'Update customer data' })
+  @ApiNoContentResponse({ description: 'Customer updated successfully' })
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateCustomerDto: UpdateCustomerDto,
@@ -46,6 +57,8 @@ export class CustomersController {
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: 'Delete a customer' })
+  @ApiNoContentResponse({ description: 'Customer deleted successfully' })
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.customersService.remove(id);
   }
--- src/modules/users/customers/dto/create-customer.dto.ts
+++ src/modules/users/customers/dto/create-customer.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCustomerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'The customer name' })
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'The customer last name' })
+  readonly lastName: string;
+
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'The customer phone number' })
+  readonly phone: string;
+}
